81aee5fb374626668738e3be5a4635830f2398ff
madolche <madolcheworld@gmail.com>
2024-07-22 00:56:30 +0700

    ganti port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
